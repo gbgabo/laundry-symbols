35b77a0c28ffd40d7eb58271a15357549aa4408a
gabo <gabriel.gaboardi13@gmail.com>
2024-06-22 17:00:26 -0300

    translated filter controls

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,6 +13,15 @@ export const ui = {
     "nav.footer":
       "<p>Made with ♥ by <a class='text-cyan-400' href='https://gabo.link'>gabo</a></p><p>Check the <a class='text-cyan-400' href='https://github.com/gbgabo/laundry-symbols'>project on github</a></p>",
     "nav.search": "Search",
+
+    "filter.label": "Filter",
+    "filter.all": "All",
+    "filter.standard": "Standard",
+    "filter.nonstandard": "Non Standard",
+    "filter.standard.description":
+      "The symbols based on GINETEX® international association for textile care labelling, adopted by most of the world",
+    "filter.nonstandard.description":
+      "Care symbols from specific countries, or legacy standards not produced anymore",
   },
   "pt-br": {
     "nav.title": "Lista de Simbolos de Lavagem",
@@ -21,5 +30,14 @@ export const ui = {
     "nav.footer":
       "<p>Feito com ♥ por <a class='text-cyan-400' href='https://gabo.link'>gabo</a></p><p>Visite o <a class='text-cyan-400' href='https://github.com/gbgabo/laundry-symbols'>projeto no github</a></p>",
     "nav.search": "Pesquisar",
+
+    "filter.label": "Filtrar",
+    "filter.all": "Todos",
+    "filter.standard": "Padrão",
+    "filter.nonstandard": "Não Padrão",
+    "filter.standard.description":
+      "The symbols based on GINETEX® international association for textile care labelling, adopted by most of the world",
+    "filter.nonstandard.description":
+      "Care symbols from specific countries, or legacy standards not produced anymore",
   },
 } as const;
